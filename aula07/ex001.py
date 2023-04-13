1608c2ef7f8a76c9d1e82c707ca3da53772df9cb
steph4nn <110504296+steph4nn@users.noreply.github.com>
2023-04-13 11:09:25 -0300

    resolvendo questoes de for e while

---
+++ aula07/ex001.py
@@ -0,0 +1,2 @@
+for i in range (50,301,5):
+    print(f'{i}, ao cubo {i**3} e ao quadrado {i*i}')
---
+++ aula07/ex002.py
@@ -0,0 +1,10 @@
+num1 = int(input('Digite um número: '))
+i = 1
+soma = 0
+
+while i < num1:
+    soma = soma + i
+    i = i + 1
+
+print(soma)
+
